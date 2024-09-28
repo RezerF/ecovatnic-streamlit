b94e10e1d1867716d994863715f3db5f85266f48
Pavel Plahin <p.plahin@omp.ru>
2024-09-28 12:10:55 +0300

    add auth for app

--- ecoapp.py
+++ ecoapp.py
@@ -5,6 +5,39 @@ from insulation_calc.calculator import CommonCalculator, EcovataCalculator
 from insulation_calc.common.constants import Plotnost
 from insulation_calc.common.table_row import TableRow
 
+import hmac
+import streamlit as st
+
+
+# auth streamlit
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+
 st.write("""
 # Калькулятор утепления Эковатой
 
